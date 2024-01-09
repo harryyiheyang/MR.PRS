d171811577bcc153faf36327a30eaf62d3611962
harryiheyang <harry.y.yang@foxmail.com>
2024-01-09 01:38:09 -0500

    add function

--- R/cisMR_PRS.R
+++ R/cisMR_PRS.R
@@ -78,7 +78,8 @@ cisMR_PRS=function(outcomefile, CHR, BPcenter, BPtol, eQTL_list, prscsxpath, pli
     system(glue("./plink --bfile {bfile} --score {prs_file_dir}/{NAM[i]}_EUR_pst_eff_a1_b0.5_phiauto_chr{CHR}.txt 2 4 6 header sum --out {prs_file_dir}/{NAM[i]}"))
   }
   if(is.null(pleiotropy[1])!=1){
-  write.table(pleiotropy,file = "{prs_file_dir}/snplist.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+  write.table(pleiotropy,"{prs_file_dir}/snplist.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+  system(glue("./plink --bfile {bfile} --extract {prs_file_dir}/snplist.txt --recode --out {prs_file_dir}/mydata_selected_snps"))
   Ple=fread("{prs_file_dir}/mydata_selected_snps.ped")%>%as.data.frame(.)
   names(Ple)[7:ncol(Ple)]=pleiotropy
   Ple=Ple[which(Ple$ID%in%indMR),]
